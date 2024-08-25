094c2cb70948a751a6a9cf40a3ce9ef9cb7836c9
Aleksa Jovanovic <aleksa.jovanovic.97@gmail.com>
2024-08-25 19:13:49 +0200

    initalized spring boot project

---
+++ src/main/java/rs/edu/code/spring_git_demo/SpringGitDemoApplication.java
@@ -0,0 +1,13 @@
+package rs.edu.code.spring_git_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/edu/code/spring_git_demo/SpringGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.edu.code.spring_git_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
